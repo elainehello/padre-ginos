30fb47ac33f3652ec8243025057d6b250ee110a3
elaine <195331169+elainehello@users.noreply.github.com>
2025-11-13 02:00:10 +0100

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,50 @@
 import { createRoot } from "react-dom/client"; // go to the lib and bring createRoot
 import Pizza from "./Pizza";
-
+import { StrictMode } from "react";
 
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza name="The Pepperoni Pizza" description="Some juicy pizza"/>
-      <Pizza name="The Napolitana Pizza" description="Delicious roast tomato greek cheese"/>
-      <Pizza name="The Pineapple greatest Pizza" description="Juicy and flavory experience"/>
-      <Pizza name="The Magic 4 cheese Pizza" description="Best cheeses from the world"/>
-      <Pizza name="The Vegetarian Pizza" description="Finest veggies"/>
-      <Pizza name="The Mushroom Pizza" description="Fresh and something else"/>
+      <Pizza
+        name="The Pepperoni Pizza"
+        description="Some juicy pizza"
+        image="/public/pizzas/pepperoni.webp"
+      />
+      <Pizza
+        name="The Napolitana Pizza"
+        description="Delicious roast tomato greek cheese"
+        image="/public/pizzas/pepperoni.webp"
+      />
+      <Pizza
+        name="The Pineapple greatest Pizza"
+        description="Juicy and flavory experience"
+        image="/public/pizzas/pepperoni.webp"
+      />
+      <Pizza
+        name="The Magic 4 cheese Pizza"
+        description="Best cheeses from the world"
+        image="/public/pizzas/pepperoni.webp"
+      />
+      <Pizza
+        name="The Vegetarian Pizza"
+        description="Finest veggies"
+        image="/public/pizzas/pepperoni.webp"
+      />
+      <Pizza
+        name="The Mushroom Pizza"
+        description="Fresh and something else"
+        image="/public/pizzas/pepperoni.webp"
+      />
     </div>
   );
 };
 
 const container = document.getElementById("root"); // type HTMLElement || null
 const root = createRoot(container); // create react root object
-root.render(<App/>); // elements - instance associated
+root.render(
+  // elements - instance associated
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -1,12 +1,13 @@
 // react component
 
 const Pizza = (props) => {
-    return (
-        <div className="pizza">
-            <h1>{props.name}</h1>
-            <p>{props.description}</p>
-        </div>
-    );
+  return (
+    <div className="pizza">
+      <h1>{props.name}</h1>
+      <p>{props.description}</p>
+      <img src={props.image} alt={props.name} />
+    </div>
+  );
 };
 
 export default Pizza;
